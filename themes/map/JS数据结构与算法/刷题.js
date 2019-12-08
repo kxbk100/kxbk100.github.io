8cbb80a9569b9b18ffeef54ce2dbf91495db60a2
fengtianxiang <fengtianxiang@youzan.com>
2019-12-08 19:38:35 +0100

    2019-12-08 19:38:32

--- themes/map/JS数据结构与算法/刷题.js
+++ themes/map/JS数据结构与算法/刷题.js
@@ -1,10 +1,88 @@
-var name = 'The Window';
-var object = {
-  name: 'My Object',
-  getNameFunc: function() {
-    return function() {
-      return this.name;
-    };
+const hasPath = (matrix, rows, cols, path) => {
+  const pathLength = 0;
+  const visited = new Array(rows * cols);
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      // 遍历，遍历的点为起点。
+      if (
+        hasPathCore(matrix, rows, cols, row, col, path, pathLength, visited)
+      ) {
+        return true;
+      }
+    }
   }
+  return false;
 };
-console.log(object.getNameFunc()()); //"The Window"（在非严格模式下）
+
+const hasPathCore = (
+  matrix,
+  rows,
+  cols,
+  row,
+  col,
+  path,
+  pathLength,
+  visited
+) => {
+  let hasPath = false;
+  if (pathLength === path.length) return true;
+  if (
+    row >= 0 &&
+    row < rows &&
+    col >= 0 &&
+    col < cols &&
+    matrix[row * cols + col] === path[pathLength] &&
+    !visited[row * cols + col]
+  ) {
+    ++pathLength;
+    visited[row * cols + col] = true;
+    // 因为||为短路运算符，只要第一个满足就会返回，而不会去计算后面的，所以有些路径可以不用去走。
+    hasPath =
+      hasPathCore(
+        matrix,
+        rows,
+        cols,
+        row - 1,
+        col,
+        path,
+        pathLength,
+        visited
+      ) ||
+      hasPathCore(
+        matrix,
+        rows,
+        cols,
+        row,
+        col - 1,
+        path,
+        pathLength,
+        visited
+      ) ||
+      hasPathCore(
+        matrix,
+        rows,
+        cols,
+        row + 1,
+        col,
+        path,
+        pathLength,
+        visited
+      ) ||
+      hasPathCore(matrix, rows, cols, row, col + 1, path, pathLength, visited);
+    if (!hasPath) {
+      --pathLength;
+      visited[row * cols + col] = false;
+    }
+  }
+  return hasPath;
+};
+
+const movingCount = (k, rows, cols) => {
+  if (k < 0 || rows <= 0 || cols <= 0) {
+    return false;
+  }
+
+  const visited = new Array(rows * cols);
+  let count = movingCountCore(k, rows, cols, 0, 0, visited);
+  return count;
+};
