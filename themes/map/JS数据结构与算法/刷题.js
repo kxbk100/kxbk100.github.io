28a7cdf890cbfbf0156072d0b518e458e82be29b
fengtianxiang <fengtianxiang@youzan.com>
2020-01-06 21:16:03 +0100

    2020-01-06 21:16:02

--- themes/map/JS数据结构与算法/刷题.js
+++ themes/map/JS数据结构与算法/刷题.js
@@ -1,32 +1,16 @@
-const maxProductAfterCutting = length => {
-  // 初始值就直接返回
-  if (length < 2) return 0;
-  if (length === 2) return 1;
-  if (length === 3) return 2;
+const Power = (base, exponent) => {
+  if (exponent === 0) return 0;
+  if (exponent === 1) return base;
 
-  // 直接赋值
-  const product = [];
-  product[0] = 0;
-  product[1] = 1;
-  product[2] = 2;
-  product[3] = 3;
+  let isNegative = false;
 
-  // 求之后数组各项的最大值
-  // 第一个for，自下而上
-  for (let i = 4; i <= length; i++) {
-    let max = 0;
-
-    for (let j = 1; j <= i / 2; j++) {
-      let product = product[j] * product[i - j];
-
-      if (max < product) {
-        max = product;
-      }
-      product[i] = max;
-    }
+  if (exponent < 0) {
+    isNegative = true;
+    exponent = -exponent;
   }
 
-  max = product[length];
+  let pow = Power(base * base, parseInt(exponent) >> 1);
+  if (parseInt(exponent) & 1 !== 0) pow = pow * base;
 
-  return max;
+  return isNegative ? 1 / pow : pow;
 };
